88dc8c47149862675b90edd38cf7678f7ae9d222
Sun Shiyong <sunshiyong8340@126.com>
2018-08-21 17:17:26 +0800

    完成预期功能

--- race/utils.js
+++ race/utils.js
@@ -1,36 +1,60 @@
 const carsData = [
-  {id: 0, name: 'car1', time: 0, distance: 0},
-  {id: 1, name: 'car2', time: 0, distance: 0},
-  {id: 2, name: 'car3', time: 0, distance: 0},
-  {id: 3, name: 'car4', time: 0, distance: 0},
-  {id: 4, name: 'car5', time: 0, distance: 0},
+  {id: 0, name: 'car1', div: null, time: 0, isFinished: false},
+  {id: 1, name: 'car2', div: null, time: 0, isFinished: false},
+  {id: 2, name: 'car3', div: null, time: 0, isFinished: false},
+  {id: 3, name: 'car4', div: null, time: 0, isFinished: false},
+  {id: 4, name: 'car5', div: null, time: 0, isFinished: false},
 ]
+carsData.forEach(carData => {
+  carData.div = document.getElementById(carData.name)
+})
+
+let startTime = null
+
 const distance = 400
 
-const getCarPosition = car => car.getBoundingClientRect().left - 28
+const getCarPosition = car => car.div.getBoundingClientRect().left - 28
 
 const move = car => {
-  const distance = Math.random() + parseFloat(car.style.left)
-  car.style.left = distance + 'px'
+  const distanceToLeft = Math.random()*2 + getCarPosition(car)
+  car.div.style.left = distanceToLeft + 'px'
 }
 
 const isFinished = car => {
-  if(getCarPosition(car) > distance) {
-    return true
-  }
+  if(getCarPosition(car) > distance) return true
+}
+const isAllFinished = carsData => {
+  return carsData.reduce((result, car) => {
+    return result&&car.isFinished
+  }, true)
+}
+
+const outPutResults = () => {
+  carsData.sort((a,b) => a.time - b.time)
+  document.getElementById('results').innerHTML = carsData.map((car, index) =>
+    `第${index+1}名是 ${car.name}，用时 ${car.time}豪秒`
+  ).join('<br/>')
 }
 
 const race = car => {
-  if(isFinished(car)) return
+  if(isFinished(car)) {
+    car.isFinished = true
+    car.time = new Date() - startTime
+    if(isAllFinished(carsData)){
+      outPutResults()
+      return
+    }
+    return
+  }
   move(car)
-  setTimeout(() => {race(car)}, 10)
+  setTimeout(() => {race(car)}, 5)
 }
 
 const start = () => {
-  carsData.forEach(carData => {
-    const car = document.getElementById(carData.name)
+  startTime = new Date()
+  carsData.forEach(car => 
     race(car)
-  })
+  )
 }
 
 const button = document.getElementById('button')
